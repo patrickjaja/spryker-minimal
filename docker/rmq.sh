2a7b7f56bad6eb890112205b127b82540fc01496
patrickjaja <patrickjaja@gmail.com>
2019-01-06 16:28:46 +0100

    removed docker

--- docker/rmq.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-docker exec -i spryker-minimal-rabbitmq rabbitmqctl add_vhost /DE_development_zed
-docker exec -i spryker-minimal-rabbitmq rabbitmqctl add_user DE_development mate20mg
-docker exec -i spryker-minimal-rabbitmq rabbitmqctl set_user_tags DE_development administrator
-docker exec -i spryker-minimal-rabbitmq rabbitmqctl set_permissions -p /DE_development_zed DE_development ".*" ".*" ".*"
-docker exec -i spryker-minimal-rabbitmq rabbitmqctl add_user admin mate20mg
-docker exec -i spryker-minimal-rabbitmq rabbitmqctl set_user_tags admin administrator
-docker exec -i spryker-minimal-rabbitmq rabbitmqctl set_permissions -p /DE_development_zed admin ".*" ".*" ".*"
